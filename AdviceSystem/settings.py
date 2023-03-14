6d8c7792c5f06b7f7344e7aacc9b69761240d2da
Andrey Limasov <a.limasov@samsung.com>
2023-03-14 18:20:05 +0300

    Docker integrate

--- AdviceSystem/settings.py
+++ AdviceSystem/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-7mwnrhae#u5c1k5k1nfo761)exlx@=onhy)$yp^@-zwd*zlebs
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM python:3.9-alpine
+FROM nickgryg/alpine-pandas
 
 WORKDIR /app
 
